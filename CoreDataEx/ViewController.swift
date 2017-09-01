4dbf89d292bfa0a1114b2f93d540b8debcf7b313
Deeksha Shenoy <deekshashenoy@Deekshas-Mac-mini.local>
2017-09-01 18:37:45 +0530

    Initial Commit

---
+++ CoreDataEx/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreDataEx
+//
+//  Created by Deeksha Shenoy on 01/09/17.
+//  Copyright © 2017 Deeksha Shenoy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
